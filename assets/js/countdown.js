0369cf7859149ff557f0189c71446aa203f7fb38
narahc321 <tanguturucharan@gmail.com>
2018-07-13 00:30:12 +0000

    new_countdown

--- assets/js/countdown.js
+++ assets/js/countdown.js
@@ -1,35 +1,46 @@
-<script type="text/javascript">
-    function countdown(){
+function getTimeRemaining(endtime) {
+  var t = Date.parse(endtime) - Date.parse(new Date());
+  var seconds = Math.floor((t / 1000) % 60);
+  var minutes = Math.floor((t / 1000 / 60) % 60);
+  var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+  var days = Math.floor(t / (1000 * 60 * 60 * 24));
+  return {
+    'total': t,
+    'days': days,
+    'hours': hours,
+    'minutes': minutes,
+    'seconds': seconds
+  };
+}
 
-        this.values = {
-          hours  : this.$.hours.parent().attr('data-init-value'),
-        minutes: this.$.minutes.parent().attr('data-init-value'),
-        seconds: this.$.seconds.parent().attr('data-init-value'),
-    };
-        var now = new Date();
-        var eventDate = new Date(2018, 8, 29, 8 ,20, 0);
-        var currentTime = new now.getTime();
-        var eventTime = new evenDate.getTime();
+function initializeClock(id, endtime) {
+  var clock = document.getElementById(id);
+  var daysSpan = clock.querySelector('.days');
+  var hoursSpan = clock.querySelector('.hours');
+  var minutesSpan = clock.querySelector('.minutes');
+  var secondsSpan = clock.querySelector('.seconds');
+
+  function updateClock() {
+    var t = getTimeRemaining(endtime);
+
+    daysSpan.innerHTML = t.days;
+    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+    if (t.total <= 0) {
+      clearInterval(timeinterval);
+    }
+  }
+
+  updateClock();
+  var timeinterval = setInterval(updateClock, 1000);
+}
+var now = new Date();
+        var eventDate = new Date(2018, 7, 29 , 8 ,20);
+        var currentTime = now.getTime();
+        var eventTime = eventDate.getTime();
         var remTime = eventTime - currentTime;
 
-        var s = Math.floor(remTime / 1000);
-        var m = Math.floor(s / 60);
-        var h = Math.floor(m / 60);
-        var d = Math.floor(h / 24);
-
-        h %= 24;
-        m %= 60;
-        s %= 60;
-
-        h = (h < 10) ? "0" + h : h;
-        m = (m < 10) ? "0" + m : m;
-        s = (s < 10) ? "0" + s : s;
-
-        document.getElementById("days").textContent = d;
-        document.getElementById("days").innerText = d;
-        document.getElementById("hours").textContent = h;
-        document.getElementById("minutes").textContent = m;
-        document.getElementById("seconds").textContent = s;
-    }
-    countdown();
-</script>
+var deadline = new Date(Date.parse(new Date()) +remTime);
+initializeClock('clockdiv', deadline);
